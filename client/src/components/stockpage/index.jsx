128a2a9acffedda8a6f1af5ce81f315241572c21
PedroRozas <p.rozasmateluna@uandresbello.edu>
2022-11-22 16:01:44 -0300

    added a new page

--- client/src/components/stockpage/index.jsx
+++ client/src/components/stockpage/index.jsx
@@ -1,26 +1,57 @@
 import "./stockstyles.css";
 
 function StockPage() {
-    return (
-        <div className="stock_container">
-           <div className="stock_form_container">
-            <form className="meds_form">
-            <input
-              type="text"
-              placeholder="Buscar por nombre"
-              name="paciente"
-              onChange=""
-              value=""
-              required
-              className="input"
-            />
-             <hr/>
-             <h1>asd</h1>
-            </form>
-           </div> 
-        </div>
-    )
+  return (
+    <div className="stock_container">
+      <h1 color="white">Consultar Stock</h1>
+        <form className="meds_form">
+          <input
+            type="text"
+            placeholder="Buscar por nombre"
+            name="paciente"
+            onChange=""
+            value=""
+            required
+            className="input"
+          />
+          <button type="submit" className="stock_btn">
+              Consultar
+            </button>
+          <div className="rowconsult">
+            <div className="textconsult">MEDICAMENTO: </div> <div className="textconsult">hola</div> 
+          </div>
+          <div className="rowconsult">
+            <div className="textconsult">CÓDIGO:</div> <div className="textconsult">hola</div> 
+          </div>
+          <div className="rowconsult">
+            <div className="textconsult">TIPO: </div> <div className="textconsult">hola</div> 
+          </div>
+          <div className="rowconsult">
+            <div className="textconsult">FABRICANTE: </div> <div className="textconsult">hola</div> 
+          </div>
+          <div className="rowconsult">
+            <div className="textconsult">CANTIDAD</div> <div className="textconsult">hola</div> 
+          </div>
+          <div className="rowconsult">
+            <div className="textconsult">GRAMAJE: </div> <div className="textconsult">hola</div> 
+          </div>
+          <div className="rowconsult">
+            <div className="textconsult">BIOEQUIVALENTE</div> <div className="textconsult">hola</div> 
+          </div>
+          <div className="rowconsult">
+            <div className="textconsult">UND. STOCK: </div> <div className="textconsult">hola</div> 
+          </div>
+          <div className="rowconsult">
+            <div className="textconsult">UND. RESERVADAS: </div> <div className="textconsult">hola</div> 
+          </div>
+          
+          
+            
+          
+        </form>
+      
+    </div>
+  );
 }
 
 export default StockPage;
-        
